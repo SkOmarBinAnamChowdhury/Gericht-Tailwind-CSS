ceda11d479024e359ba1fb6a8419747f62a32104
Sk Omar Bin Anam Chowdhury <skomarbinanam@gmail.com>
2024-11-08 00:13:26 +0600

    pushণ

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      cormorant_upright: 'Cormorant Upright, serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
